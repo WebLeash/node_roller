d59ab3b15bec786d9868e0ded257ecef452198b4
Mr Nathan C Stott <nathan.stott@sky.com>
2022-06-05 10:34:01 +0100

    generic
    make code generic

--- launcher.sh
+++ launcher.sh
@@ -4,15 +4,15 @@
 #runnner with creds.
 # Bring into play the AWS Keys
 
-export BASE_DIR="/builds/insaas/tools/insaas-ami-build"
+export BASE_DIR="/builds/<your build path>"
 
 
-export AWS_DEFAULT_REGION="eu-west-2" # Chane to eu-west-1 for ROI
+export AWS_DEFAULT_REGION="<Your default region>" # Chane to eu-west-1 for ROI
 
 show_help() {
 cat << EOF
 Usage: ${0##*/}  -e ENVIRONMENT
-only using development currently. NOT set-up for Production
+
 
 EOF
 }
@@ -39,15 +39,15 @@ fi
 
 vault ()
 {
-    echo "Vault not white listed NO CONNECTION"; return
-    export VAULT_ADDR=https://vault.secrets.digital.coveahosted.co.uk
+    echo "Not leveraging vault, below code goes someway towards a vault integration".
+    export VAULT_ADDR=<Vault domain>
     export VAULT_TOKEN=""
 
     if [[ "${VAULT_TOKEN:-}" == "" ]]; then
        echo "Vault token is not set"
        exit 1
     fi
-    QUERY="ci-insaas-dev/platform/test-nathan"
+    QUERY="<vault path>"
     ./vault kv get -field=kubeconfig "${QUERY}" >/code/.kube/kubeconfig
 
 
@@ -56,11 +56,11 @@ vault ()
 config (){
     echo "Generating kubeconfig"
     mkdir -p ${BASE_DIR}/.kube
-    cat ${insaas_box_config} >${BASE_DIR}/.kube/kubeconfig
+    cat ${kube_config} >${BASE_DIR}/.kube/kubeconfig
     export KUBECONFIG=${BASE_DIR}/.kube/kubeconfig 
     echo "Setting up AWS Keys...."
-    export AWS_ACCESS_KEY_ID=${INSAAS_DEV_BUILD_KEY}
-    export AWS_SECRET_ACCESS_KEY=${INSAAS_DEV_BUILD_SECRET_KEY}
+    export AWS_ACCESS_KEY_ID=${instore file secret store}
+    export AWS_SECRET_ACCESS_KEY=${secret file secret store}
 
 
        
